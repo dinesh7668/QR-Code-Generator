2a4497325e8dd2ee57a6df19f28fb538fba51ea0
Dinesh Pal Mourya <dineshpal7668@gmail.com>
2025-10-07 16:24:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const qrText = document.querySelector("#qr-text");
+const generateBtn = document.querySelector("#generate");
+const downloadBtn = document.querySelector("#download");
+const sizes = document.querySelector("#size");
+const qrContainer = document.querySelector(".qr-body");
+
+let size = sizes.value;
+let qr;
+
+generateBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  generateQRCode();
+});
+
+sizes.addEventListener("change", (e) => {
+  size = e.target.value;
+  generateQRCode();
+});
+
+function generateQRCode() {
+  qrContainer.innerHTML = "";
+  qr = new QRCode(qrContainer, {
+    text: qrText.value,
+    width: size,
+    height: size,
+    colorLight: "#fff",
+    colorDark: "#19086fff",
+  });
+}
+
+downloadBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let img = qrContainer.querySelector("img");
+  if (img) {
+    let imgSrc = img.src;
+    let a = document.createElement("a");
+    a.href = imgSrc;
+    a.download = "QR_Code_By_Dinesh.png";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  } else {
+    alert("Please generate a QR code first.");
+  }
+});
